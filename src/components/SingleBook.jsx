5ec867be2cd4efe98a7f1e8374c2708e6012fa41
MrMax01 <massimo.mauro00@gmail.com>
2023-09-07 17:00:18 +0200

    try to post

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -14,6 +14,21 @@ class SingleBook extends Component {
   handleChange = (propertyName, propertyValue) => {
     this.setState({ review: { ...this.state.review, [propertyName]: propertyValue } });
   };
+
+  postOn = (event, bookId) => {
+    event.preventDefault();
+    this.setState({ review: { ...this.state.review, idBook: bookId } });
+    fetch("https://striveschool-api.herokuapp.com/api/comments/", {
+      method: "POST",
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjkxOTEwYmNhMDAwMTQ1ODQwMTMiLCJpYXQiOjE2OTQwOTc5NzEsImV4cCI6MTY5NTMwNzU3MX0.GdQsz182yV7pgRdapt0yK0usVcEy9HbUeZlMhOhMAHQ",
+
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state.review),
+    }).then((response) => console.log(response.ok));
+  };
   render() {
     const book = this.props.book;
     return (
@@ -21,7 +36,11 @@ class SingleBook extends Component {
         <Card.Img variant="top" src={book.img} height={300} className="object-fit-cover" />
         <Card.Body>
           <Card.Title className="noCapo">{book.title}</Card.Title>
-          {/* <Form>
+          <Form
+            onSubmit={(event) => {
+              this.postOn(event, book.asin);
+            }}
+          >
             <Form.Group className="mb-3">
               <Form.Label>Nome:</Form.Label>
               <Form.Control
@@ -29,12 +48,14 @@ class SingleBook extends Component {
                 placeholder="your name..."
                 value={this.state.review.name}
                 onChange={(event) => this.handleChange("name", event.target.value)}
+                required
               />
             </Form.Group>
             <Form.Select
               aria-label="Default select"
               value={this.state.review.rate}
               onChange={(event) => this.handleChange("rate", event.target.value)}
+              required
             >
               <option value="">rate us...</option>
               <option>1</option>
@@ -50,12 +71,13 @@ class SingleBook extends Component {
                 rows={3}
                 value={this.state.review.comment}
                 onChange={(event) => this.handleChange("comment", event.target.value)}
+                required
               />
             </Form.Group>
             <Button variant="primary" type="submit">
               Submit
             </Button>
-          </Form> */}
+          </Form>
         </Card.Body>
       </Card>
     );
